106ba433644e3bb367e1991d92409edb4c9a9acf
neya-mul <neyamulislam946@gmail.com>
2026-07-12 19:35:52 +0600

    bug fixed

--- script.ts
+++ script.ts
@@ -24,12 +24,15 @@ const client = new MongoClient(process.env.MONGO_URI as string, {
         deprecationErrors: true,
     }
 });
-async function run() {
+
+
+
+// async function run() {
 
 
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
         // // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         // console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -365,8 +368,8 @@ async function run() {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
-}
-run().catch(console.dir);
+// }
+// run().catch(console.dir);
 
 
 
